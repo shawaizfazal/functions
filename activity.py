17a4d86d7b7e2404e1234ffc19b7254b7c6fc9f4
shawaizfazal <shazmaxfazal@gmail.com>
2026-07-16 18:42:14 +0500

    keywords

---
+++ activity.py
@@ -0,0 +1,7 @@
+z=input("enter any word")
+for i in z :
+ if(i=="z"):
+    print("z is found ")
+    break
+else:
+    print("z is not found")
---
+++ activity2.py
@@ -0,0 +1,11 @@
+for l in range(10):
+  if l % 20==0:
+      print("twist")
+  elif l % 15==0:
+    pass
+  elif l %5==0:
+      print("fizz")
+  elif l %3==0:
+      print("buzz")
+  else:
+      print("l")
